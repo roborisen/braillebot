773e7adeb89520f1eefb9d76eeb3ac227cbd532c
roborisen <62860479+roborisen@users.noreply.github.com>
2025-08-30 16:57:39 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,23 +1,25 @@
 braillebot.setupBrailleBot()
+music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 basic.forever(function () {
     braillebot.readColorSensor()
     braillebot.showColorKey(braillebot.getColorKey())
     if (braillebot.isColorDetected(braillebot.inKey.RED_KEY)) {
-        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Action)
+        braillebot.playTwoNotes(braillebot.Note.G4, braillebot.Note.B4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.TurnRight)
         braillebot.turnRight()
         braillebot.lineTrackingToNextColor()
     } else if (braillebot.isColorDetected(braillebot.inKey.BLUE_KEY)) {
-        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Action)
+        braillebot.playTwoNotes(braillebot.Note.G4, braillebot.Note.E4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.TurnLeft)
         braillebot.turnLeft()
         braillebot.lineTrackingToNextColor()
     } else if (braillebot.isColorDetected(braillebot.inKey.YELLOW_KEY)) {
-        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Action)
+        braillebot.playTwoNotes(braillebot.Note.G4, braillebot.Note.G4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.UTurn)
         braillebot.uTurn()
         braillebot.lineTrackingToNextColor()
     } else if (braillebot.isColorDetected(braillebot.inKey.ORANGE_KEY)) {
+        basic.pause(500)
         braillebot.showIcon(braillebot.Icons.MoveForward)
         braillebot.lineTrackingSkipAndNextColor()
     } else if (braillebot.isColorDetected(braillebot.inKey.CYAN_KEY)) {
@@ -26,13 +28,13 @@ basic.forever(function () {
         braillebot.showIcon(braillebot.Icons.MoveForward)
         braillebot.lineTrackingSkipAndNextColor()
     } else if (braillebot.isColorDetected(braillebot.inKey.PINK_KEY)) {
-        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Action)
+        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C5, braillebot.Action.Action)
         braillebot.gripperOpenBlock(braillebot.Opening.MovingOpen)
     } else if (braillebot.isColorDetected(braillebot.inKey.VIOLET_KEY)) {
-        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Action)
+        braillebot.playTwoNotes(braillebot.Note.C5, braillebot.Note.C4, braillebot.Action.Action)
         braillebot.gripperCloseBlock(braillebot.Closing.MovingClose)
     } else if (braillebot.isColorDetected(braillebot.inKey.GREEN_KEY)) {
-        braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Stop)
+        braillebot.playTwoNotes(braillebot.Note.C5, braillebot.Note.C5, braillebot.Action.Stop)
     } else {
 
     }
