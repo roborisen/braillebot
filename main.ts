c6ddbf09e3571ff3197a1cf003cc6d54a1d07803
roborisen <62860479+roborisen@users.noreply.github.com>
2025-09-12 14:17:26 +0900

    main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -440,7 +440,7 @@ namespace braillebot {
             basic.pause(130)
             colorKey = detectColorKey()
 
-            if ((red + green + blue) < 4000) {
+            if ((red + green + blue) < 3500) { //2025-09-12
                 whitecheck = false
             }
 
