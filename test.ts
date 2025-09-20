53f1fa38afa5c72990210fdd62f3456d8865b331
roborisen <62860479+roborisen@users.noreply.github.com>
2025-09-20 20:14:46 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -2,42 +2,42 @@ braillebot.setupBrailleBot()
 music.play(music.builtinPlayableSoundEffect(soundExpression.hello), music.PlaybackMode.UntilDone)
 basic.forever(function () {
     braillebot.readColorSensor()
-    braillebot.showColorKey(braillebot.getColorKey())
-    if (braillebot.isColorDetected(braillebot.inKey.RED_KEY)) {
+    braillebot.showColorKey(braillebot.readColorKey())
+    if (braillebot.isColorDetected(braillebot.InKey.RedKey)) {
         braillebot.playTwoNotes(braillebot.Note.G4, braillebot.Note.B4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.TurnRight)
         braillebot.turnRight()
         braillebot.lineTrackingToNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.BLUE_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.BlueKey)) {
         braillebot.playTwoNotes(braillebot.Note.G4, braillebot.Note.E4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.TurnLeft)
         braillebot.turnLeft()
         braillebot.lineTrackingToNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.YELLOW_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.YellowKey)) {
         braillebot.playTwoNotes(braillebot.Note.G4, braillebot.Note.G4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.UTurn)
         braillebot.uTurn()
         braillebot.lineTrackingToNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.ORANGE_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.OrangeKey)) {
         basic.pause(500)
         braillebot.showIcon(braillebot.Icons.MoveForward)
         braillebot.lineTrackingSkipAndNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.CYAN_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.CyanKey)) {
         braillebot.setEchoOn()
         braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.MoveForward)
         braillebot.lineTrackingSkipAndNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.PINK_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.PinkKey)) {
         braillebot.playTwoNotes(braillebot.Note.C4, braillebot.Note.C5, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.GripperOpen)
         braillebot.gripperOpenBlock(braillebot.Opening.MovingOpen)
         braillebot.lineTrackingToNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.VIOLET_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.VioletKey)) {
         braillebot.playTwoNotes(braillebot.Note.C5, braillebot.Note.C4, braillebot.Action.Action)
         braillebot.showIcon(braillebot.Icons.GripperClose)
         braillebot.gripperCloseBlock(braillebot.Closing.MovingClose)
         braillebot.lineTrackingToNextColor()
-    } else if (braillebot.isColorDetected(braillebot.inKey.GREEN_KEY)) {
+    } else if (braillebot.isColorDetected(braillebot.InKey.GreenKey)) {
         braillebot.playTwoNotes(braillebot.Note.C5, braillebot.Note.C5, braillebot.Action.Stop)
         braillebot.showIcon(braillebot.Icons.Stop)
     } else {
